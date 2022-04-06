802fbbc05eaaec9ab06eaec2b330b96b9b58ad31
Tarlan Ismayilsoy <terlan_ismayilsoy@hotmail.com>
2022-04-06 11:57:26 +0400

    Created the project. Added a launch screen.

---
+++ breakingBadWiki/breakingBadWiki/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  breakingBadWiki
+//
+//  Created by Tarlan Ismayilsoy on 06.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ breakingBadWiki/breakingBadWiki/breakingBadWikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  breakingBadWikiApp.swift
+//  breakingBadWiki
+//
+//  Created by Tarlan Ismayilsoy on 06.04.22.
+//
+
+import SwiftUI
+
+@main
+struct breakingBadWikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
